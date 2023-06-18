c2b006dada671ebc5b2179adeeed5404aa32828a
Kiselev Danil <dabikis2855@gmail.com>
2023-06-18 21:54:59 +0600

    [+] sendSimpleMessage() call

--- src/main/java/com/example/courseworkmailer/controller/RabbitMqListener.java
+++ src/main/java/com/example/courseworkmailer/controller/RabbitMqListener.java
@@ -1,14 +1,24 @@
 package com.example.courseworkmailer.controller;
 
+import com.example.courseworkmailer.service.MailService;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
+@EnableRabbit
+@Component
 public class RabbitMqListener {
-    Logger logger = LoggerFactory.getLogger(RabbitMqListener.class);
+    MailService mail;
 
     @RabbitListener(queues = "mainQueue")
     public void listen(String message) {
+        Logger logger = LoggerFactory.getLogger(RabbitMqListener.class);
         logger.info("Received message: {}", message);
+        //TODO: receiver email receiving;
+        mail.sendSimpleMessage("dabikis2855@gmail.com", "Coursework Application", message);
     }
 }
