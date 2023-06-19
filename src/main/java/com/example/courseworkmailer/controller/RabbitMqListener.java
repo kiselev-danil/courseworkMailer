7f8bf61d224f0526906bdb5e551a4bc5de003101
Kiselev Danil <dabikis2855@gmail.com>
2023-06-19 16:16:58 +0600

    [+] now receive RabbitMqMessage class, instead of string

--- src/main/java/com/example/courseworkmailer/controller/RabbitMqListener.java
+++ src/main/java/com/example/courseworkmailer/controller/RabbitMqListener.java
@@ -1,24 +1,33 @@
 package com.example.courseworkmailer.controller;
 
+import com.example.courseworkmailer.entity.RabbitMqMessage;
 import com.example.courseworkmailer.service.MailService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
-@EnableRabbit
+
+//@EnableRabbit
 @Component
 public class RabbitMqListener {
     MailService mail;
 
+    //@RabbitListener(queues = "mainQueue")
+    RabbitTemplate rabbitTemplate;
+
+    public RabbitMqListener(RabbitTemplate rabbitTemplate, MailService mail) {
+        this.mail = mail;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @RabbitListener(queues = "mainQueue")
-    public void listen(String message) {
+    public void listen(RabbitMqMessage message) {
         Logger logger = LoggerFactory.getLogger(RabbitMqListener.class);
-        logger.info("Received message: {}", message);
-        //TODO: receiver email receiving;
-        mail.sendSimpleMessage("dabikis2855@gmail.com", "Coursework Application", message);
+        logger.info("Received message: {}", message.toString());
+        mail.sendSimpleMessage(message.to(), message.from(), message.subject(), message.text());
     }
 }
